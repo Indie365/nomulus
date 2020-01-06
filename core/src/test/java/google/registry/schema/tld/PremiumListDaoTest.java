b2fd65fb067058bc581658377b0048a0524e948a
Ben McIlwain <mcilwain@google.com>
2020-01-06 15:57:44 -0500

    Relax premium list existence check to allow Cloud SQL migration
    We need to be able to simultaneously update premium lists that already exist in
    Datastore and create them in Cloud SQL (because they haven't been migrated over
    yet). This temporarily relaxes the existence check for Cloud SQL so that
    "updates" will work even when the list doesn't yet exist there.

--- core/src/test/java/google/registry/schema/tld/PremiumListDaoTest.java
+++ core/src/test/java/google/registry/schema/tld/PremiumListDaoTest.java
@@ -35,6 +35,7 @@ import java.math.BigDecimal;
 import java.util.Optional;
 import org.joda.money.CurrencyUnit;
 import org.joda.money.Money;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -126,6 +127,7 @@ public class PremiumListDaoTest {
   }
 
   @Test
+  @Ignore
   public void update_throwsWhenListDoesntExist() {
     IllegalArgumentException thrown =
         assertThrows(
