24fb8b15c4020f20e0f955a8313ce7f5947001fd
Legina Chen <legina@google.com>
2020-06-15 18:54:18 +0000

    Add review changes

--- core/src/test/java/google/registry/persistence/converter/LocalDateConverterTest.java
+++ core/src/test/java/google/registry/persistence/converter/LocalDateConverterTest.java
@@ -1,30 +1,38 @@
+// Copyright 2020 The Nomulus Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package google.registry.persistence.converter;
 
+import static com.google.common.truth.Truth.assertThat;
 import static google.registry.persistence.transaction.TransactionManagerFactory.jpaTm;
 
 import google.registry.model.ImmutableObject;
+import google.registry.persistence.VKey;
 import google.registry.persistence.transaction.JpaTestRules;
 import google.registry.persistence.transaction.JpaTestRules.JpaUnitTestRule;
 import google.registry.schema.replay.EntityTest;
-import org.joda.time.LocalDate;
-import org.joda.time.format.ISODateTimeFormat;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.PersistenceException;
-
-import static com.google.common.truth.Truth.assertThat;
-import static org.junit.Assert.assertThrows;
+import org.joda.time.LocalDate;
+import org.joda.time.format.ISODateTimeFormat;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 /** Unit tests for {@link LocalDateConverter}. */
-@RunWith(JUnit4.class)
 public class LocalDateConverterTest {
 
-  @Rule
+  @RegisterExtension
   public final JpaUnitTestRule jpaRule =
       new JpaTestRules.Builder()
           .withEntityClass(LocalDateConverterTestEntity.class)
@@ -59,46 +67,20 @@ public class LocalDateConverterTest {
     instantiateAndPersistTestEntity();
     LocalDateConverterTestEntity retrievedEntity =
         jpaTm()
-            .transact(
-                () -> jpaTm().getEntityManager().find(LocalDateConverterTestEntity.class, "id"));
+            .transact(() -> jpaTm().load(VKey.createSql(LocalDateConverterTestEntity.class, "id")));
     assertThat(retrievedEntity.date.toString()).isEqualTo("2020-06-10");
   }
 
   @Test
-  public void invalidLocalDate() {
+  public void roundTripConversion() {
+    instantiateAndPersistTestEntity();
     jpaTm()
         .transact(
             () ->
                 jpaTm()
                     .getEntityManager()
                     .createNativeQuery(
-                        "INSERT INTO \"LocalDateConverterTestEntity\" (name, date) VALUES('id', 'XXXX')")
-                    .executeUpdate());
-    assertThrows(
-        PersistenceException.class,
-        () ->
-            jpaTm()
-                .transact(
-                    () ->
-                        jpaTm()
-                            .getEntityManager()
-                            .find(LocalDateConverterTestEntity.class, "id")
-                            .date));
-  }
-
-  @Test
-  public void roundTripConversion() {
-    instantiateAndPersistTestEntity();
-    assertThat(
-            jpaTm()
-                .transact(
-                    () ->
-                        jpaTm()
-                            .getEntityManager()
-                            .createNativeQuery(
-                                "SELECT date FROM \"LocalDateConverterTestEntity\" WHERE name = 'id'")
-                            .getResultList()))
-        .containsExactly("2020-06-10");
+                        "SELECT date FROM \"LocalDateConverterTestEntity\" WHERE name = 'id'"));
     LocalDateConverterTestEntity persisted =
         jpaTm()
             .transact(
@@ -108,7 +90,7 @@ public class LocalDateConverterTest {
 
   private void instantiateAndPersistTestEntity() {
     LocalDateConverterTestEntity entity = new LocalDateConverterTestEntity(date);
-    jpaTm().transact(() -> jpaTm().getEntityManager().persist(entity));
+    jpaTm().transact(() -> jpaTm().saveNew(entity));
   }
 
   /** Override entity name to avoid the nested class reference. */
