989c1835556da184453a06554e8739a445b32978
Shicong Huang <shicong@google.com>
2020-07-29 20:22:52 -0400

    Remove logic for PollMessage and resolve comments

--- core/src/test/java/google/registry/tools/ResaveEntitiesWithUniqueIdCommandTest.java
+++ core/src/test/java/google/registry/tools/ResaveEntitiesWithUniqueIdCommandTest.java
@@ -35,11 +35,11 @@ import google.registry.model.poll.PollMessage;
 import google.registry.model.reporting.HistoryEntry;
 import google.registry.model.transfer.DomainTransferData;
 import org.joda.money.Money;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 /** Unit tests for {@link ResaveEntitiesWithUniqueIdCommand}. */
-public class ResaveEntitiesWithUniqueIdCommandTest
+class ResaveEntitiesWithUniqueIdCommandTest
     extends CommandTestCase<ResaveEntitiesWithUniqueIdCommand> {
 
   DomainBase domain;
@@ -47,8 +47,8 @@ public class ResaveEntitiesWithUniqueIdCommandTest
   PollMessage.Autorenew autorenewToResave;
   BillingEvent.OneTime billingEventToResave;
 
-  @Before
-  public void setUp() {
+  @BeforeEach
+  void setUp() {
     createTld("foobar");
     domain = persistActiveDomain("foo.foobar");
     historyEntry = persistHistoryEntry(domain);
@@ -57,7 +57,7 @@ public class ResaveEntitiesWithUniqueIdCommandTest
   }
 
   @Test
-  public void resaveBillingEvent_succeeds() throws Exception {
+  void resaveBillingEvent_succeeds() throws Exception {
     runCommand(
         "--force", writeToNamedTmpFile("keypath.txt", getKeyPathLiteral(billingEventToResave)));
 
@@ -73,7 +73,7 @@ public class ResaveEntitiesWithUniqueIdCommandTest
   }
 
   @Test
-  public void resaveBillingEvent_failsWhenReferredByDomain() throws Exception {
+  void resaveBillingEvent_failsWhenReferredByDomain() throws Exception {
     persistResource(
         domain
             .asBuilder()
