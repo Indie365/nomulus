ee4f4ff6b8af12721a96643de59253fa505f8542
Gus Brodman <gbrodman@google.com>
2021-01-15 15:40:17 -0500

    Convert (most) HistoryEntry ofy calls to tm
    As part of this change, it was necessary to do changes in the JPATM that
    are similar (but the opposite) of the changes that we did in
    DatastoreTM with regards to converting HistoryEntries to and from the
    *History classes.
    
    We leave the ofy() calls in the MapReduce ResaveAllHistoryEntriesAction
    for now; that can be converted during the Beam pipeline transition.
    
    Some other tests required registrar-name fixes as well -- because
    *History objects have a foreign key on the Registrar table, we have to
    use a "real" registrar name in tests.

--- core/src/main/java/google/registry/tools/GetHistoryEntriesCommand.java
+++ core/src/main/java/google/registry/tools/GetHistoryEntriesCommand.java
@@ -16,6 +16,8 @@ package google.registry.tools;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static google.registry.model.ofy.ObjectifyService.ofy;
+import static google.registry.persistence.transaction.TransactionManagerFactory.jpaTm;
+import static google.registry.persistence.transaction.TransactionManagerFactory.tm;
 import static google.registry.util.DateTimeUtils.END_OF_TIME;
 import static google.registry.util.DateTimeUtils.START_OF_TIME;
 import static google.registry.util.PreconditionsUtils.checkArgumentNotNull;
@@ -23,16 +25,22 @@ import static org.joda.time.DateTimeZone.UTC;
 
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
+import com.google.common.collect.Iterables;
 import google.registry.model.EppResource;
+import google.registry.model.contact.ContactHistory;
+import google.registry.model.domain.DomainHistory;
+import google.registry.model.host.HostHistory;
 import google.registry.model.reporting.HistoryEntry;
 import google.registry.persistence.VKey;
 import google.registry.tools.CommandUtilities.ResourceType;
 import google.registry.xml.XmlTransformer;
+import java.util.Locale;
 import org.joda.time.DateTime;
 
 /** Command to show history entries. */
-@Parameters(separators = " =",
-  commandDescription = "Show history entries that occurred in a given time range")
+@Parameters(
+    separators = " =",
+    commandDescription = "Show history entries that occurred in a given time range")
 final class GetHistoryEntriesCommand implements CommandWithRemoteApi {
 
   @Parameter(
@@ -45,33 +53,26 @@ final class GetHistoryEntriesCommand implements CommandWithRemoteApi {
       description = "Only show history entries that occurred at or before this time")
   private DateTime before = END_OF_TIME;
 
-  @Parameter(
-    names = "--type",
-    description = "Resource type.")
+  @Parameter(names = "--type", description = "Resource type.")
   private ResourceType type;
 
-  @Parameter(
-    names = "--id",
-    description = "Foreign key of the resource.")
+  @Parameter(names = "--id", description = "Foreign key of the resource.")
   private String uniqueId;
 
   @Override
   public void run() {
-    VKey<? extends EppResource> parentKey = null;
+    Iterable<? extends HistoryEntry> historyEntries;
     if (type != null || uniqueId != null) {
       checkArgument(
           type != null && uniqueId != null,
           "If either of 'type' or 'id' are set then both must be");
-      parentKey = type.getKey(uniqueId, DateTime.now(UTC));
+      VKey<? extends EppResource> parentKey = type.getKey(uniqueId, DateTime.now(UTC));
       checkArgumentNotNull(parentKey, "Invalid resource ID");
+      historyEntries = loadEntriesForSingleResource(parentKey);
+    } else {
+      historyEntries = loadAllHistoryEntries();
     }
-    for (HistoryEntry entry :
-        (parentKey == null
-                ? ofy().load().type(HistoryEntry.class)
-                : ofy().load().type(HistoryEntry.class).ancestor(parentKey.getOfyKey()))
-            .order("modificationTime")
-            .filter("modificationTime >=", after)
-            .filter("modificationTime <=", before)) {
+    for (HistoryEntry entry : historyEntries) {
       System.out.printf(
           "Client: %s\nTime: %s\nClient TRID: %s\nServer TRID: %s\n%s\n",
           entry.getClientId(),
@@ -82,5 +83,73 @@ final class GetHistoryEntriesCommand implements CommandWithRemoteApi {
               ? String.format("[no XML stored for %s]\n", entry.getType())
               : XmlTransformer.prettyPrint(entry.getXmlBytes()));
     }
+  }
+
+  private Iterable<? extends HistoryEntry> loadEntriesForSingleResource(
+      VKey<? extends EppResource> parentKey) {
+    if (tm().isOfy()) {
+      return ofy()
+          .load()
+          .type(HistoryEntry.class)
+          .ancestor(parentKey.getOfyKey())
+          .order("modificationTime")
+          .filter("modificationTime >=", after)
+          .filter("modificationTime <=", before);
+    } else {
+      Class<? extends HistoryEntry> clazz =
+          type.equals(ResourceType.CONTACT)
+              ? ContactHistory.class
+              : type.equals(ResourceType.HOST) ? HostHistory.class : DomainHistory.class;
+      String repoIdFieldName = String.format("%sRepoId", type.name().toLowerCase(Locale.ENGLISH));
+      String queryString =
+          String.format(
+              "SELECT entry FROM %s entry WHERE entry.modificationTime >= :afterTime AND "
+                  + "entry.modificationTime <= :beforeTime AND entry.%s = :parentKey",
+              clazz.getSimpleName(), repoIdFieldName);
+      return jpaTm()
+          .transact(
+              () ->
+                  jpaTm()
+                      .getEntityManager()
+                      .createQuery(queryString, clazz)
+                      .setParameter("afterTime", after)
+                      .setParameter("beforeTime", before)
+                      .setParameter("parentKey", parentKey.getSqlKey().toString())
+                      .getResultList());
+    }
+  }
+
+  private Iterable<HistoryEntry> loadAllHistoryEntries() {
+    if (tm().isOfy()) {
+      return ofy()
+          .load()
+          .type(HistoryEntry.class)
+          .order("modificationTime")
+          .filter("modificationTime >=", after)
+          .filter("modificationTime <=", before);
+    } else {
+      return jpaTm()
+          .transact(
+              () ->
+                  Iterables.concat(
+                      getAllHistoryEntriesFromTable(DomainHistory.class),
+                      getAllHistoryEntriesFromTable(HostHistory.class),
+                      getAllHistoryEntriesFromTable(ContactHistory.class)));
+    }
+  }
+
+  private Iterable<? extends HistoryEntry> getAllHistoryEntriesFromTable(
+      Class<? extends HistoryEntry> clazz) {
+    return jpaTm()
+        .getEntityManager()
+        .createQuery(
+            String.format(
+                "SELECT entry FROM %s entry WHERE entry.modificationTime >= :afterTime AND "
+                    + "entry.modificationTime <= :beforeTime",
+                clazz.getSimpleName()),
+            clazz)
+        .setParameter("afterTime", after)
+        .setParameter("beforeTime", before)
+        .getResultList();
   }
 }
