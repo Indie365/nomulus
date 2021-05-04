bd8b5dc7225224e3b7dbd440b72372ce2473ceba
Gus Brodman <gbrodman@google.com>
2021-05-04 12:53:16 -0400

    Combine the two Lock classes into one class
    This allows us to remove the DAO and to just treat locks the same as we
    would treat any other object -- generically grabbing them from the
    transaction manager.
    
    We do not need to be concerned about the changeover between Datastore
    and SQL because we assume that any such changeover will require
    sufficient downtime that any currently-valid acquired locks will expire
    during the downtime. Otherwise, we could get into a situation where an
    action has acquired a particular lock in Datastore but not SQL.

--- core/src/main/java/google/registry/model/server/Lock.java
+++ core/src/main/java/google/registry/model/server/Lock.java
@@ -15,8 +15,9 @@
 package google.registry.model.server;
 
 import static com.google.common.base.Preconditions.checkArgument;
-import static google.registry.persistence.transaction.TransactionManagerFactory.ofyTm;
+import static google.registry.persistence.transaction.TransactionManagerFactory.tm;
 import static google.registry.util.DateTimeUtils.isAtOrAfter;
+import static google.registry.util.PreconditionsUtils.checkArgumentNotNull;
 
 import com.google.auto.value.AutoValue;
 import com.google.common.annotations.VisibleForTesting;
@@ -29,12 +30,17 @@ import google.registry.model.ImmutableObject;
 import google.registry.model.annotations.NotBackedUp;
 import google.registry.model.annotations.NotBackedUp.Reason;
 import google.registry.persistence.VKey;
-import google.registry.schema.replay.DatastoreOnlyEntity;
+import google.registry.schema.replay.DatastoreAndSqlEntity;
 import google.registry.util.RequestStatusChecker;
 import google.registry.util.RequestStatusCheckerImpl;
 import java.io.Serializable;
 import java.util.Optional;
 import javax.annotation.Nullable;
+import javax.persistence.Column;
+import javax.persistence.IdClass;
+import javax.persistence.PostLoad;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 import org.joda.time.DateTime;
 import org.joda.time.Duration;
 
@@ -49,20 +55,29 @@ import org.joda.time.Duration;
  */
 @Entity
 @NotBackedUp(reason = Reason.TRANSIENT)
-public class Lock extends ImmutableObject implements DatastoreOnlyEntity, Serializable {
+@javax.persistence.Entity
+@Table
+@IdClass(Lock.LockId.class)
+public class Lock extends ImmutableObject implements DatastoreAndSqlEntity, Serializable {
 
   private static final long serialVersionUID = 756397280691684645L;
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 
-  /** Disposition of locking, for monitoring. */
-  enum LockState { IN_USE, FREE, TIMED_OUT, OWNER_DIED }
+  /** The scope of a lock that is not specific to a single tld. */
+  static final String GLOBAL = "GLOBAL";
 
-  @VisibleForTesting
-  static LockMetrics lockMetrics = new LockMetrics();
+  /** Disposition of locking, for monitoring. */
+  enum LockState {
+    IN_USE,
+    FREE,
+    TIMED_OUT,
+    OWNER_DIED
+  }
+
+  @VisibleForTesting static LockMetrics lockMetrics = new LockMetrics();
 
   /** The name of the locked resource. */
-  @Id
-  String lockId;
+  @Transient @Id String lockId;
 
   /**
    * Unique log ID of the request that owns this lock.
@@ -73,40 +88,43 @@ public class Lock extends ImmutableObject implements DatastoreOnlyEntity, Serial
    * <p>See {@link RequestStatusCheckerImpl#getLogId} for details about how it's created in
    * practice.
    */
+  @Column(nullable = false)
   String requestLogId;
 
   /** When the lock can be considered implicitly released. */
+  @Column(nullable = false)
   DateTime expirationTime;
 
-  public String getRequestLogId() {
-    return requestLogId;
-  }
+  /** When was the lock acquired. Used for logging. */
+  @Column(nullable = false)
+  DateTime acquiredTime;
+
+  /** The resource name used to create the lock. */
+  @Column(nullable = false)
+  @javax.persistence.Id
+  String resourceName;
+
+  /** The tld used to create the lock, or GLOBAL if it's cross-TLD. */
+  @Column(nullable = false)
+  @javax.persistence.Id
+  String tld;
 
   public DateTime getExpirationTime() {
     return expirationTime;
   }
 
-  public DateTime getAcquiredTime() {
-    return acquiredTime;
+  @PostLoad
+  void postLoad() {
+    lockId = makeLockId(resourceName, tld);
   }
 
-  /** When was the lock acquired. Used for logging. */
-  DateTime acquiredTime;
-
-  /** The resource name used to create lockId. */
-  String resourceName;
-
-  /** The tld used to create lockId. */
-  @Nullable
-  String tld;
-
   /**
-   * Create a new {@link Lock} for the given resource name in the specified tld (which can be null
-   * for cross-tld locks).
+   * Create a new {@link Lock} for the given resource name in the specified tld (or in the GLOBAL
+   * namespace).
    */
-  public static Lock create(
+  private static Lock create(
       String resourceName,
-      @Nullable String tld,
+      String tld,
       String requestLogId,
       DateTime acquiredTime,
       Duration leaseLength) {
@@ -123,7 +141,7 @@ public class Lock extends ImmutableObject implements DatastoreOnlyEntity, Serial
     return instance;
   }
 
-  private static String makeLockId(String resourceName, @Nullable String tld) {
+  private static String makeLockId(String resourceName, String tld) {
     return String.format("%s-%s", tld, resourceName);
   }
 
@@ -186,21 +204,23 @@ public class Lock extends ImmutableObject implements DatastoreOnlyEntity, Serial
       Duration leaseLength,
       RequestStatusChecker requestStatusChecker,
       boolean checkThreadRunning) {
-    String lockId = makeLockId(resourceName, tld);
+    String namespace = tld != null ? tld : GLOBAL;
+    String lockId = makeLockId(resourceName, namespace);
     // It's important to use transactNew rather than transact, because a Lock can be used to control
     // access to resources like GCS that can't be transactionally rolled back. Therefore, the lock
     // must be definitively acquired before it is used, even when called inside another transaction.
     AcquireResult acquireResult =
-        ofyTm()
-            .transactNew(
+        tm().transactNew(
                 () -> {
-                  DateTime now = ofyTm().getTransactionTime();
+                  DateTime now = tm().getTransactionTime();
 
                   // Checking if an unexpired lock still exists - if so, the lock can't be acquired.
                   Lock lock =
-                      ofyTm()
-                          .loadByKeyIfPresent(
-                              VKey.createOfy(Lock.class, Key.create(Lock.class, lockId)))
+                      tm().loadByKeyIfPresent(
+                              VKey.create(
+                                  Lock.class,
+                                  new LockId(resourceName, namespace),
+                                  Key.create(Lock.class, lockId)))
                           .orElse(null);
                   if (lock != null) {
                     logger.atInfo().log(
@@ -220,43 +240,48 @@ public class Lock extends ImmutableObject implements DatastoreOnlyEntity, Serial
                   }
 
                   Lock newLock =
-                      create(resourceName, tld, requestStatusChecker.getLogId(), now, leaseLength);
+                      create(
+                          resourceName,
+                          namespace,
+                          requestStatusChecker.getLogId(),
+                          now,
+                          leaseLength);
                   // Locks are not parented under an EntityGroupRoot (so as to avoid write
-                  // contention) and
-                  // don't need to be backed up.
-                  ofyTm().putWithoutBackup(newLock);
+                  // contention) and don't need to be backed up.
+                  tm().putWithoutBackup(newLock);
 
                   return AcquireResult.create(now, lock, newLock, lockState);
                 });
 
     logAcquireResult(acquireResult);
-    lockMetrics.recordAcquire(resourceName, tld, acquireResult.lockState());
+    lockMetrics.recordAcquire(resourceName, namespace, acquireResult.lockState());
     return Optional.ofNullable(acquireResult.newLock());
   }
 
   /** Release the lock. */
   public void release() {
     // Just use the default clock because we aren't actually doing anything that will use the clock.
-    ofyTm()
-        .transact(
+    tm().transact(
             () -> {
               // To release a lock, check that no one else has already obtained it and if not
               // delete it. If the lock in Datastore was different then this lock is gone already;
               // this can happen if release() is called around the expiration time and the lock
               // expires underneath us.
               Lock loadedLock =
-                  ofyTm()
-                      .loadByKeyIfPresent(
-                          VKey.createOfy(Lock.class, Key.create(Lock.class, lockId)))
+                  tm().loadByKeyIfPresent(
+                          VKey.create(
+                              Lock.class,
+                              new LockId(resourceName, tld),
+                              Key.create(Lock.class, lockId)))
                       .orElse(null);
               if (Lock.this.equals(loadedLock)) {
-                // Use noBackupOfy() so that we don't create a commit log entry for deleting the
-                // lock.
+                // Use deleteWithoutBackup() so that we don't create a commit log entry for deleting
+                // the lock.
                 logger.atInfo().log("Deleting lock: %s", lockId);
-                ofyTm().deleteWithoutBackup(Lock.this);
+                tm().deleteWithoutBackup(Lock.this);
 
                 lockMetrics.recordRelease(
-                    resourceName, tld, new Duration(acquiredTime, ofyTm().getTransactionTime()));
+                    resourceName, tld, new Duration(acquiredTime, tm().getTransactionTime()));
               } else {
                 logger.atSevere().log(
                     "The lock we acquired was transferred to someone else before we"
@@ -267,5 +292,20 @@ public class Lock extends ImmutableObject implements DatastoreOnlyEntity, Serial
                     "Not deleting lock: %s - someone else has it: %s", lockId, loadedLock);
               }
             });
+  }
+
+  static class LockId extends ImmutableObject implements Serializable {
+
+    String resourceName;
+
+    String tld;
+
+    // Required for Hibernate
+    private LockId() {}
+
+    LockId(String resourceName, String tld) {
+      this.resourceName = checkArgumentNotNull(resourceName, "The resource name cannot be null");
+      this.tld = tld;
+    }
   }
 }
