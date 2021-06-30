f5ef5d09452a34558338737fa64dc0ccc753a651
Lai Jiang <jianglai@msn.com>
2021-06-30 09:14:26 -0400

    Remove the use of the GCS API provided from the GAE SDK
    The API provided by the GAE SDK will not be available outside GAE
    runtime. This presents a problem when we migrate off of GAE. More
    pressingly, the RDE pipeline migration to Beam requires that we write to
    GCS on GCE. Previously we were able to sidestep the issue by delegating
    the writes to FileIO provided by Beam, which knows how to write to GCS.
    However the RDE pipeline cannot use FileIO directly as it needs to write
    to multiple files in one go and explicit use of GCS API is needed.
    
    An unfortunate side effect of the API migration is that the new testing
    library contains a bug which makes serializing GcsUtils impossible. It
    is fixed upstream but not released yet. The fix has been backported for
    the time being.

--- core/src/main/java/google/registry/backup/ReplayCommitLogsToSqlAction.java
+++ core/src/main/java/google/registry/backup/ReplayCommitLogsToSqlAction.java
@@ -23,10 +23,10 @@ import static org.joda.time.Duration.standardHours;
 
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.Key;
-import com.google.appengine.tools.cloudstorage.GcsFileMetadata;
-import com.google.appengine.tools.cloudstorage.GcsService;
+import com.google.cloud.storage.BlobInfo;
 import com.google.common.collect.ImmutableList;
 import com.google.common.flogger.FluentLogger;
+import google.registry.gcs.GcsUtils;
 import google.registry.model.common.DatabaseMigrationStateSchedule;
 import google.registry.model.common.DatabaseMigrationStateSchedule.MigrationState;
 import google.registry.model.common.DatabaseMigrationStateSchedule.ReplayDirection;
@@ -46,7 +46,6 @@ import google.registry.util.Clock;
 import google.registry.util.RequestStatusChecker;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.channels.Channels;
 import java.util.Optional;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletResponse;
@@ -65,14 +64,13 @@ public class ReplayCommitLogsToSqlAction implements Runnable {
   static final String PATH = "/_dr/task/replayCommitLogsToSql";
 
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
-  private static final int BLOCK_SIZE =
-      1024 * 1024; // Buffer 1mb at a time, for no particular reason.
+
   private static final Duration LEASE_LENGTH = standardHours(1);
   // Stop / pause where we are if we've been replaying for more than five minutes to avoid GAE
   // request timeouts
   private static final Duration REPLAY_TIMEOUT_DURATION = Duration.standardMinutes(5);
 
-  @Inject GcsService gcsService;
+  @Inject GcsUtils gcsUtils;
   @Inject Response response;
   @Inject RequestStatusChecker requestStatusChecker;
   @Inject GcsDiffFileLister diffLister;
@@ -128,10 +126,10 @@ public class ReplayCommitLogsToSqlAction implements Runnable {
     DateTime fromTime = jpaTm().transact(() -> SqlReplayCheckpoint.get().plusMillis(1));
     // If there's an inconsistent file set, this will throw IllegalStateException and the job
     // will try later -- this is likely because an export hasn't finished yet.
-    ImmutableList<GcsFileMetadata> commitLogFiles =
+    ImmutableList<BlobInfo> commitLogFiles =
         diffLister.listDiffFiles(fromTime, /* current time */ null);
     int processedFiles = 0;
-    for (GcsFileMetadata metadata : commitLogFiles) {
+    for (BlobInfo metadata : commitLogFiles) {
       // One transaction per GCS file
       jpaTm().transact(() -> processFile(metadata));
       processedFiles++;
@@ -145,31 +143,26 @@ public class ReplayCommitLogsToSqlAction implements Runnable {
     logger.atInfo().log("Replayed %d commit log files to SQL successfully.", processedFiles);
   }
 
-  private void processFile(GcsFileMetadata metadata) {
+  private void processFile(BlobInfo metadata) {
     logger.atInfo().log(
-        "Processing commit log file %s of size %d B.",
-        metadata.getFilename(), metadata.getLength());
-    try (InputStream input =
-        Channels.newInputStream(
-            gcsService.openPrefetchingReadChannel(metadata.getFilename(), 0, BLOCK_SIZE))) {
+        "Processing commit log file %s of size %d B.", metadata.getName(), metadata.getSize());
+    try (InputStream input = gcsUtils.openInputStream(metadata.getBlobId())) {
       // Load and process the Datastore transactions one at a time
       ImmutableList<ImmutableList<VersionedEntity>> allTransactions =
           CommitLogImports.loadEntitiesByTransaction(input);
       logger.atInfo().log(
           "Replaying %d transactions from commit log file %s.",
-          allTransactions.size(), metadata.getFilename());
+          allTransactions.size(), metadata.getName());
       allTransactions.forEach(this::replayTransaction);
       // if we succeeded, set the last-seen time
-      DateTime checkpoint =
-          DateTime.parse(
-              metadata.getFilename().getObjectName().substring(DIFF_FILE_PREFIX.length()));
+      DateTime checkpoint = DateTime.parse(metadata.getName().substring(DIFF_FILE_PREFIX.length()));
       SqlReplayCheckpoint.set(checkpoint);
       logger.atInfo().log(
           "Replayed %d transactions from commit log file %s.",
-          allTransactions.size(), metadata.getFilename());
+          allTransactions.size(), metadata.getName());
     } catch (IOException e) {
       throw new RuntimeException(
-          "Errored out while replaying commit log file " + metadata.getFilename(), e);
+          "Errored out while replaying commit log file " + metadata.getName(), e);
     }
   }
 
