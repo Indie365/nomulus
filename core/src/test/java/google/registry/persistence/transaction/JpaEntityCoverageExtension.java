d71cff83bde9c1eb298b9f2ebe225baf148cba2d
Gus Brodman <gbrodman@google.com>
2021-08-05 12:24:14 -0400

    Store DatabaseMigrationSchedule in SQL instead of Datastore
    This requires messing around with some of the JPA unit test rule
    creation since it requires saving / retrieving the schedule pretty much
    always (which itself includes the hstore extension).

--- core/src/test/java/google/registry/persistence/transaction/JpaEntityCoverageExtension.java
+++ core/src/test/java/google/registry/persistence/transaction/JpaEntityCoverageExtension.java
@@ -42,6 +42,9 @@ public class JpaEntityCoverageExtension implements BeforeEachCallback, AfterEach
   // TODO(weiminyu): update this set when entities written to Cloud SQL and tests are added.
   private static final ImmutableSet<String> IGNORE_ENTITIES =
       ImmutableSet.of(
+          // DatabaseMigrationStateSchedule tests clean up after themselves so we can't tell that
+          // any entities were persisted
+          "DatabaseMigrationStateSchedule",
           "DelegationSignerData",
           "DesignatedContact",
           "GracePeriod",
