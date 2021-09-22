74f1f46152c37a57d596a7d07caaf07d9f1ed238
Lai Jiang <jianglai@msn.com>
2021-09-22 11:25:23 -0400

    WIP

--- core/src/main/java/google/registry/beam/common/RegistryPipelineWorkerInitializer.java
+++ core/src/main/java/google/registry/beam/common/RegistryPipelineWorkerInitializer.java
@@ -46,13 +46,24 @@ public class RegistryPipelineWorkerInitializer implements JvmInitializer {
     if (environment == null || environment.equals(RegistryEnvironment.UNITTEST)) {
       return;
     }
-    //logger.atInfo().log("Setting up RegistryEnvironment: %s", environment);
-    //environment.setup();
+    logger.atInfo().log("Setting up RegistryEnvironment: %s", environment);
+    environment.setup();
+    RegistryPipelineComponent component = toRegistryPipelineComponent(registryOptions);
+    // Eagerly load the project ID, which causes the config file to be memoized.
+    component.getProjectId();
     Lazy<JpaTransactionManager> transactionManagerLazy =
         toRegistryPipelineComponent(registryOptions).getJpaTransactionManager();
     TransactionManagerFactory.setJpaTmOnBeamWorker(transactionManagerLazy::get);
+    // Masquerade all threads on beam workers as GAE threads, see DatastoreEntityExtension on why.
     ApiProxy.setEnvironmentForCurrentThread(PlaceholderEnvironment.get());
     ApiProxy.setEnvironmentFactory(PlaceholderEnvironment::get);
     auditedOfy();
+    // Unset the environment so that calls to IdService.allocateId() will not call Datastore (which
+    // will fail because 1) we spoofed the GAE environment above, and 2) the call to Datastore is
+    // via an API in GAE SDK, so it only works in GAE. Getting allocated a fake ID is fine because
+    // the only instance where we need to do this is in the RDE pipeline where we clone a domain
+    // at the snapshot time. The ID is an internal construct that is not exposed in the RDE deposit,
+    // and it is not re-saved back to the database.
+    RegistryEnvironment.UNITTEST.setup();
   }
 }
