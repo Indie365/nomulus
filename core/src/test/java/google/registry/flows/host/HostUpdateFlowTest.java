1dcf07492d2b16e71c0319ff6b3f7455325e10ca
Lai Jiang <jianglai@google.com>
2022-09-13 09:58:35 -0400

    Refactor ForeignKeyIndex into ForeignKeyUtils

--- core/src/test/java/google/registry/flows/host/HostUpdateFlowTest.java
+++ core/src/test/java/google/registry/flows/host/HostUpdateFlowTest.java
@@ -69,14 +69,15 @@ import google.registry.flows.host.HostUpdateFlow.CannotRemoveSubordinateHostLast
 import google.registry.flows.host.HostUpdateFlow.CannotRenameExternalHostException;
 import google.registry.flows.host.HostUpdateFlow.HostAlreadyExistsException;
 import google.registry.flows.host.HostUpdateFlow.RenameHostToExternalRemoveIpException;
+import google.registry.model.ForeignKeyUtils;
 import google.registry.model.domain.Domain;
 import google.registry.model.eppcommon.StatusValue;
 import google.registry.model.host.Host;
-import google.registry.model.index.ForeignKeyIndex;
 import google.registry.model.reporting.HistoryEntry;
 import google.registry.model.tld.Registry;
 import google.registry.model.transfer.DomainTransferData;
 import google.registry.model.transfer.TransferStatus;
+import google.registry.persistence.VKey;
 import google.registry.testing.DatabaseHelper;
 import google.registry.testing.TaskQueueHelper.TaskMatcher;
 import javax.annotation.Nullable;
@@ -184,9 +185,8 @@ class HostUpdateFlowTest extends ResourceFlowTestCase<HostUpdateFlow, Host> {
     Host renamedHost = doSuccessfulTest();
     assertThat(renamedHost.isSubordinate()).isTrue();
     assertDnsTasksEnqueued("ns1.example.tld", "ns2.example.tld");
-    ForeignKeyIndex<Host> oldFkiAfterRename =
-        ForeignKeyIndex.load(Host.class, oldHostName(), clock.nowUtc());
-    assertThat(oldFkiAfterRename).isNull();
+    VKey<Host> oldVKeyAfterRename = ForeignKeyUtils.load(Host.class, oldHostName(), clock.nowUtc());
+    assertThat(oldVKeyAfterRename).isNull();
   }
 
   @Test
