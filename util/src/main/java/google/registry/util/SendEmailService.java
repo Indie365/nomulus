4d4fd8a4f609e80a9740f44473a5da73ee493ded
ptkach <ptkach@google.com>
2022-10-04 20:40:04 +0000

    Limit environments allowed to send emails out

--- util/src/main/java/google/registry/util/SendEmailService.java
+++ util/src/main/java/google/registry/util/SendEmailService.java
@@ -16,6 +16,9 @@ package google.registry.util;
 
 import static com.google.common.collect.Iterables.toArray;
 
+import com.google.common.base.Ascii;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.flogger.FluentLogger;
 import com.google.common.net.MediaType;
 import google.registry.util.EmailMessage.Attachment;
 import java.io.IOException;
@@ -44,6 +47,10 @@ public class SendEmailService {
   private final Retrier retrier;
   private final TransportEmailSender transportEmailSender;
 
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+  private static final ImmutableSet<String> ALLOWED_ENVS =
+      ImmutableSet.of("PRODUCTION", "UNITTEST");
+
   @Inject
   SendEmailService(Retrier retrier, TransportEmailSender transportEmailSender) {
     this.retrier = retrier;
@@ -55,37 +62,46 @@ public class SendEmailService {
    * on transient failures.
    */
   public void sendEmail(EmailMessage emailMessage) {
-    retrier.callWithRetry(
-        () -> {
-          Message msg =
-              new MimeMessage(
-                  Session.getDefaultInstance(new Properties(), /* authenticator= */ null));
-          msg.setFrom(emailMessage.from());
-          msg.addRecipients(
-              RecipientType.TO, toArray(emailMessage.recipients(), InternetAddress.class));
-          msg.setSubject(emailMessage.subject());
+    if (!ALLOWED_ENVS.contains(
+        Ascii.toUpperCase(System.getProperty("google.registry.environment", "UNITTEST")))) {
+      logger.atInfo().log(
+          String.format(
+              "Email with subject %s... was reverted from being sent out in"
+                  + " incompatible environment",
+              emailMessage.subject().substring(0, Math.min(emailMessage.subject().length(), 15))));
+    } else {
+      retrier.callWithRetry(
+          () -> {
+            Message msg =
+                new MimeMessage(
+                    Session.getDefaultInstance(new Properties(), /* authenticator= */ null));
+            msg.setFrom(emailMessage.from());
+            msg.addRecipients(
+                RecipientType.TO, toArray(emailMessage.recipients(), InternetAddress.class));
+            msg.setSubject(emailMessage.subject());
 
-          Multipart multipart = new MimeMultipart();
-          BodyPart bodyPart = new MimeBodyPart();
-          bodyPart.setContent(
-              emailMessage.body(),
-              emailMessage.contentType().orElse(MediaType.PLAIN_TEXT_UTF_8).toString());
-          multipart.addBodyPart(bodyPart);
+            Multipart multipart = new MimeMultipart();
+            BodyPart bodyPart = new MimeBodyPart();
+            bodyPart.setContent(
+                emailMessage.body(),
+                emailMessage.contentType().orElse(MediaType.PLAIN_TEXT_UTF_8).toString());
+            multipart.addBodyPart(bodyPart);
 
-          if (emailMessage.attachment().isPresent()) {
-            Attachment attachment = emailMessage.attachment().get();
-            BodyPart attachmentPart = new MimeBodyPart();
-            attachmentPart.setContent(attachment.content(), attachment.contentType().toString());
-            attachmentPart.setFileName(attachment.filename());
-            multipart.addBodyPart(attachmentPart);
-          }
-          msg.addRecipients(RecipientType.BCC, toArray(emailMessage.bccs(), Address.class));
-          msg.addRecipients(RecipientType.CC, toArray(emailMessage.ccs(), Address.class));
-          msg.setContent(multipart);
-          msg.saveChanges();
-          transportEmailSender.sendMessage(msg);
-        },
-        IOException.class,
-        MessagingException.class);
+            if (emailMessage.attachment().isPresent()) {
+              Attachment attachment = emailMessage.attachment().get();
+              BodyPart attachmentPart = new MimeBodyPart();
+              attachmentPart.setContent(attachment.content(), attachment.contentType().toString());
+              attachmentPart.setFileName(attachment.filename());
+              multipart.addBodyPart(attachmentPart);
+            }
+            msg.addRecipients(RecipientType.BCC, toArray(emailMessage.bccs(), Address.class));
+            msg.addRecipients(RecipientType.CC, toArray(emailMessage.ccs(), Address.class));
+            msg.setContent(multipart);
+            msg.saveChanges();
+            transportEmailSender.sendMessage(msg);
+          },
+          IOException.class,
+          MessagingException.class);
+    }
   }
 }
